085ac1f68d4569af69022a34d79eadac95483b9a
JaniBT <janoscsordas072@gmail.com>
2023-12-12 13:45:45 +0100

    website updates
    A Fő weboldal majdnem teljesen készen van. Apróbb finomítások szükségesek lehetnek a későbbiekben. Továbbá, ha stílus váltásra kerülne a voks, akkor természetesen azt is kivitelezném. A többi aloldal elkészítése későbbi feladatként lett titulálva.

--- script.js
+++ script.js
@@ -13,3 +13,58 @@ $(document).ready(function () {
       localStorage.setItem('isModalShown', true);
     });
 });
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+window.onscroll = function() {
+  showBackToTopButton();
+};
+
+function showBackToTopButton() {
+  var btn = document.getElementById("backToTopBtn");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      btn.style.display = "block";
+  } else {
+      btn.style.display = "none";
+  }
+}
+
+// Animált görgetés a lap tetejére
+document.getElementById("backToTopBtn").addEventListener("click", function() {
+  scrollToTop(1000);
+});
+
+function scrollToTop(duration) {
+  var start = document.documentElement.scrollTop || document.body.scrollTop;
+  var startTime = new Date().getTime();
+
+  function scroll() {
+      var currentTime = new Date().getTime();
+      var timeElapsed = currentTime - startTime;
+
+      document.documentElement.scrollTop = document.body.scrollTop = easeInOut(timeElapsed, start, -start, duration);
+
+      if (timeElapsed < duration) {
+          requestAnimationFrame(scroll);
+      }
+  }
+
+  function easeInOut(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(scroll);
+}
+
+// Display the loader overlay until the website fully loads
+window.addEventListener('load', function () {
+  const loaderOverlay = document.querySelector('.loader-overlay');
+  loaderOverlay.style.display = 'none';
+  document.body.style.overflow = 'auto'; // Visszaállítjuk a görgetés engedélyezését
+});
